b9afd74fb1b7991c270d0a73b73bb2b0ced290cd
kamanivrunda65 <kamanivrunda65@gmail.com>
2022-11-04 13:26:37 +0530

    notes

---
+++ php/notes.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h3>
+        PHP
+    </h3>
+
+    <ul>
+        <li>PHP is a server scripting language, and a powerful tool for making dynamic and interactive Web pages.</li>
+        <li>PHP is an acronym for "PHP: Hypertext Preprocessor".</li>
+        <li> PHP is a widely-used, open source scripting language</li>
+        <li>PHP is free to download and use</li>
+        <li>It is deep enough to run large social networks!</li>
+        <li>With PHP you are not limited to output HTML. You can output images, PDF files, and even Flash movies.You can also output any text, such as XHTML and XML.</li>
+        <li> PHP remains widely used in the web development niche. The use of PHP is nearly everywhere on the web. </li>
+        <li>It is estimated that 79.1% of all websites with a recognized server-side programming language use PHP. </li>
+        <li> PHP ensures all these features are achieved through its three-tiered architecture that works in a linear fashion on browser, server, and database systems. </li>
+        <li>PHP can be used to build both static and dynamic web pages and web-based applications. Developers can manipulate the coding to reuse it in other applications too.</li>            <li> Some of the best Web Content Management Systems (CMS) managed by PHP are WordPress and its plugins, user interface of Drupal, Joomla, Facebook, MediaWiki, Silverstripe, and Digg, among others. </li>
+        <li>PHP can be for creating desktop Graphical User Interface (GUI) based applications. </li>
+        <li>its simple to create and incorporate flash elements like login-form and email form into dynamic web pages with the use of PHP.</li>
+        <li></li>
+    </ul>
+
+    <p>PHP supports HTML but HTML doesnt supports the PHP thats why we aleays create .php extension pages</p>
+
+</body>
+</html>
